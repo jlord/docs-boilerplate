376aa55c3a2d539b7df43c9d6a5d295892efb9ba
Jessica Lord <jessica.lord@github.com>
2015-07-14 11:17:03 -0700

    todo: refactor it all

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ module.exports = function docsBoilerplate (option, customConfig) {
   console.log('hello', option, customConfig)
   if (customConfig) customConfig = require(customConfig)
   if (option) parseOptions(option)
-  else writeDocs('default')
+  else getFiles('default')
 }
 
 function parseOptions (option) {
@@ -22,36 +22,46 @@ function parseOptions (option) {
   var docSet = config[option]
   // ask if should use default upon not finding
   if (!docSet) return console.log('Could not find', option, 'in config.json')
-  if (docSet.length === 1) {
-	  // it is a directory, glob all the files and write each one
-  } else if (docSet.legnth >= 1) {
-  	// it is an array of files, write each one
-  }
+  // it could be an array with a dir and a file string!
+  docSet.forEach(function (doc) {
+    var filepath = path.join(__dirname, doc)
+    fs.stat(filepath, function (err, stats) {
+      if (stats.isFile()) writeDocs(doc)
+      else if (stats.isDirectory()) {
+        var array = []
+        array.push(doc)
+        writeDocs(array)
+      }
+    })
+  })
+  // may not be a dir, may just be one filel listed.
+  // check each thing in array to see if directory or file
   // get option and match it to key in config
   // use that to get a directory of array of files
   // pass that onto writeDocs
 }
 
-function writeDocs (docs) {
-  console.log("docs", docs)
+// takes in a string for directory or array of strings for files
+function getFiles (docs) {
   if (typeof docs === 'string') {
-    // console.log(path.resolve(__dirname, docs))
     glob(path.resolve(__dirname, docs) + '/**', function (err, files) {
       if (err) return console.log(err)
-      // console.log('files', files)
-      files.forEach(function writeFile (file) {
-        if (err) return console.log(err)
-        // how to tell it not to match directories?
-        if (file.indexOf('.') < 0) return
-        var contents = fs.readFileSync(file)
-        var pathParts = file.split('/')
-        var filename = pathParts[pathParts.length - 1]
-        var outputPath = path.join(process.cwd(), filename)
-        var contents = fs.readFileSync(file)
-        fs.writeFileSync(outputPath, contents.toString())
-      })
+      writeDocs(files)
     })
+  } else if (typeof docs === 'array') {
+    console.log('array')
+    // writeDocs(docs)
   }
+}
 
-  // path may be string for directory or array for files
+function writeDocs (files) {
+  files.forEach(function writeFile (file) {
+    // how to tell it not to match directories?
+    if (file.indexOf('.') < 0) return
+    var contents = fs.readFileSync(file)
+    var pathParts = file.split('/')
+    var filename = pathParts[pathParts.length - 1]
+    var outputPath = path.join(process.cwd(), filename)
+    fs.writeFileSync(outputPath, contents.toString())
+  })
 }
